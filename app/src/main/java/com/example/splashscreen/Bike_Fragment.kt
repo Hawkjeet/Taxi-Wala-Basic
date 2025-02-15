323c448a14d473e79595e7beabe49e48b0a27352
Hawkjeet <jeetmulani2006@gmail.com>
2025-02-15 18:55:11 +0530

    Till Fragment

---
+++ app/src/main/java/com/example/splashscreen/Bike_Fragment.kt
@@ -0,0 +1,6 @@
+package com.example.splashscreen
+
+import androidx.fragment.app.Fragment
+
+class Bike_Fragment:Fragment(R.layout.bike_fragment) {
+}
---
+++ app/src/main/java/com/example/splashscreen/Rick_Fragment.kt
@@ -0,0 +1,6 @@
+package com.example.splashscreen
+
+import androidx.fragment.app.Fragment
+
+class Rick_Fragment:Fragment(R.layout.rick_fragment) {
+}
